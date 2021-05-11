d4543953a3c612125118050411821cb4b41caaeb
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-05-11 20:05:28 +0900

    Add files via upload

---
+++ 자료형,함수 연습/main.js
@@ -0,0 +1,12 @@
+// 여기에 logParticipant 함수를 작성해 주세요.
+function logParticipant(x){
+  console.log( x +'(이)가 대화에 참여했습니다.');
+}
+
+// 테스트 코드
+logParticipant('동수');
+logParticipant('윤하');
+logParticipant('재준');
+logParticipant('동훈');
+logParticipant('영희');
+logParticipant('신욱');
